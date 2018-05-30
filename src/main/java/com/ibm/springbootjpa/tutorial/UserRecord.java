31589673bd013c6d01661bc200a9da5e4ed31a0c
Andres Ceballos Sanchez <andres.ceballos@ibm.com>
2018-05-30 12:06:46 -0500

    Added User Entity

---
+++ src/main/java/com/ibm/springbootjpa/tutorial/UserRecord.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package com.ibm.springbootjpa.tutorial;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @author andressanchez
+ *
+ */
+@Entity
+public class UserRecord {
+
+	@Id
+	private int id;
+	private String name;
+	private String email;
+	
+	public UserRecord() {
+		
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
