ca336feb0db249a00b1a058de41bda0cecf2ad41
Andres Ceballos Sanchez <andres.ceballos@ibm.com>
2018-05-30 12:19:31 -0500

    Added a controller to expose the endpoints

---
+++ src/main/java/com/ibm/springbootjpa/tutorial/SpringBootJPAApplication.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.ibm.springbootjpa.tutorial;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author andressanchez
+ *
+ */
+public class SpringBootJPAApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJPAApplication.class, args);
+	}
+
+}
--- src/main/java/com/ibm/springbootjpa/tutorial/UserService.java
+++ src/main/java/com/ibm/springbootjpa/tutorial/UserService.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 	
+	@Autowired
 	private UserRepository userRepository;
 	
 	public List<UserRecord> getAllUsers() {
